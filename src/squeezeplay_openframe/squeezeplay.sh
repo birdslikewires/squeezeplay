e0627f1ca3afd9be7125b912fddcacdb698aa519
Andrew Davison <andydvsn@users.noreply.github.com>
2020-04-09 02:45:50 +0100

    Update squeezeplay.sh
    Perform timezone lookup and set system timezone via timedatectl on first run.

--- src/squeezeplay_openframe/squeezeplay.sh
+++ src/squeezeplay_openframe/squeezeplay.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-##
-## This script is a basic startup script for the SqueezePlay binary (jive) that requires a few environment variables be set.
-##
+## squeezeplay.sh v2.01 (9th April 2020)
+##  Identifies which OpenFrame device we're running on and applies some tweaks.
 
-## Change these if you changed your install path
+source /etc/lsb-release
+
+## Change this if you changed your install path
 INSTALL_DIR=/opt/squeezeplay
+
+## Change this to alter timezone server path
+TZ_SRV="https://openbeak.net/tz/lookup.php"
+
 LIB_DIR=$INSTALL_DIR/lib
 INC_DIR=$INSTALL_DIR/include
+SYS_TYP=$(cat /tmp/openframe.uid)/"squeezeplay-"$(cat $INSTALL_DIR/share/squeezeplay.version)"-"$(cat $INSTALL_DIR/share/squeezeplay.revision)"/"$(echo ${DISTRIB_DESCRIPTION,,} | sed 's/\ /\//g')"/"$(uname -r)""
 
 ## Start up
 export LD_LIBRARY_PATH=$LIB_DIR:$LD_LIBRARY_PATH
@@ -26,8 +32,19 @@ if [ -e /tmp/openframe.ver ]; then
 	OPENFRAME=`cat /tmp/openframe.ver`
 
 	if [ ! -d /home/$USER/.squeezeplay ]; then
-		 echo "Initial run, setting master audio output to full."
+		echo "Initial run checks..."
+		echo -n "Setting master audio output to full..."
 		/usr/bin/amixer -q -c 0 set "Master" 100% unmute
+		echo " done."
+		echo -n "Setting timezone..."
+		TIMEZONE=$(curl -sA "$SYS_TYP" -m 2 "$TZ_SRV")
+		if [[ "$TIMEZONE" != "" ]]; then
+			echo -n " found $TIMEZONE..."
+			sudo /usr/bin/timedatectl set-timezone $TIMEZONE
+			echo " done."
+		else
+			echo " lookup failed... retaining $(timedatectl status | grep "Time zone" | awk -F\  {'print $3'})."
+		fi
 	fi
 
 	if [ $OPENFRAME -eq 1 ]; then
